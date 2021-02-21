1615853e78fe289466dbf31b03fa5ce7200b1bb1
shaynedsouza <shaynedsouza@hotmail.com>
2021-02-21 12:32:14 +0530

    New Sprites

--- Assets/BabyBlobHandler.cs
+++ Assets/BabyBlobHandler.cs
@@ -7,14 +7,19 @@ public class BabyBlobHandler : MonoBehaviour
 
     // 0 for sticky, 1 for fire, 2 for water
     public int id = 0;
-
+    float temp;
 
 
     private void Start()
     {
-        float temp = transform.position.y + 0.2f;
-        transform.DOMoveY(temp, 1f).SetEase(Ease.OutSine).SetLoops(-1, LoopType.Yoyo);
+        temp = transform.position.y + 0.2f;
+        StartCoroutine(Hover());
+
     }
 
-
+    IEnumerator Hover()
+    {
+        yield return new WaitForSeconds(Random.Range(0f, 1f));
+        transform.DOMoveY(temp, 1f).SetEase(Ease.OutSine).SetLoops(-1, LoopType.Yoyo);
+    }
 }
